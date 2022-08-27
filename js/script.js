37c9f44080caf55992cadcc94404ee63e37c0637
alymichwoo <alyssawoods3@gmail.com>
2022-08-27 14:37:40 -0700

    Fetch and display public repos

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 //Div containing profile information
 const overview = document.querySelector(".overview");
+//Unordered list of the repos
+const repoList = document.querySelector(".repo-list");
 const username = "alymichwoo";
 
 const getUserInfo = async function () {
-    const response = await fetch (`https://api.github.com/users/${username}`);
-    const data = await response.json();
-    // console.log(data);
+    const userInfo = await fetch (`https://api.github.com/users/${username}`);
+    const data = await userInfo.json();
+    console.log(data);
     displayUserInfo(data);
 };
 
 getUserInfo();
 
+//Displays the basic user information 
 const displayUserInfo = function (data) {
     const userInfoDiv = document.createElement("div");
     userInfoDiv.classList.add("user-info");
@@ -23,6 +26,23 @@ const displayUserInfo = function (data) {
         <p><strong>Bio:</strong> ${data.bio}</p>
         <p><strong>Location:</strong> ${data.location}</p>
         <p><strong>Number of public repos:</strong> ${data.public_repos}</p>
-    </div>`
+    </div>`;
     overview.append(userInfoDiv);
+    getRepos();
+};
+
+//Fetches the repos from GitHub
+const getRepos = async function () {
+    const repoInfo = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await repoInfo.json();
+    displayRepoInfo(repoData);
+};
+
+const displayRepoInfo = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
 };
